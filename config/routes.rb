be8a9c80d52c825d94c764dccc1f311848e09ec4
valliyapan07 <122003276@sastra.ac.in>
2020-05-12 09:20:21 +0530

    added stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :menus
+  resources :users
+  resources :cartitems
+  resources :orders
+  get "/" => "home#index"
+  delete "cartitems", to: "cartitems#deleteall"
+  get "/sing-in" => "sessions#new", as: :new_sessions
+  post "/sign_in", to: "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
