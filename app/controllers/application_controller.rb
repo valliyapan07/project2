7af56ccfd229bbd8af714022d9c928f4e94d88b9
valliyapan07 <122003276@sastra.ac.in>
2020-05-16 13:50:38 +0530

    added billing clerk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,12 @@ class ApplicationController < ActionController::Base
 
     current_user_id = session[:current_user_id]
     if current_user_id
-      @current_user = User.find(current_user_id)
+      if User.find(current_user_id).role == "Billing clerk"
+        @current_user = User.where(first_name: "Walk-incustomer")
+        @current_billingclerk = User.find(current_user_id)
+      else
+        @current_user = User.find(current_user_id)
+      end
     else
       nil
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :email_id, presence: true
   validates :password, length: { in: 6..20 }
+  validates_uniqueness_of :email_id
 end
