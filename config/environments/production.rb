1d406def85489b471a2515448bd95d87d2159971
valliyapan07 <122003276@sastra.ac.in>
2020-05-18 14:18:13 +0530

    added email verification by sending mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "project2_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
